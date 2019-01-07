a92e86eba10c292321762972481efb9c753635a5
a.abobakr <a.abobakr@moto-soft.com>
2019-01-07 06:33:24 +0200

    add base architecture

--- app/src/main/java/com/andlausy/githubrepos/GithubReposApp.java
+++ app/src/main/java/com/andlausy/githubrepos/GithubReposApp.java
@@ -6,6 +6,6 @@ import dagger.android.DaggerApplication;
 public class GithubReposApp extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
+        return DaggerApplication::onCreate;
     }
 }
